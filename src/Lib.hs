98403b1c25db5e43dab06c5361e2400175b449d0
Sebastián Estrella <sestrella.me@gmail.com>
2019-02-21 22:41:23 -0500

    First implementation

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,33 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib where
+
+import Data.Type.Bool
+import Data.Type.Equality
+import GHC.TypeLits
+import Type.Reflection
+
+data FizzBuzz
+  = Fizz
+  | Buzz
+  | FizzBuzz
+  | Number Nat
+
+type ToFizzBuzz a = If (DivisibleBy a 5 && DivisibleBy a 3) 'FizzBuzz (TiFizz a)
+
+type TiFizz a = If (DivisibleBy a 3) Fizz (ToBuzz a)
+
+type ToBuzz a = If (DivisibleBy a 5) Buzz (Number a)
+
+type DivisibleBy (a :: Nat) (b :: Nat) = Mod a b == 0
+
+divisibleBy3 :: ToFizzBuzz 3 :~: Fizz
+divisibleBy3 = Refl
+
+divisibleBy5 :: ToFizzBuzz 5 :~: Buzz
+divisibleBy5 = Refl
+
+divisibleBy3And5 :: ToFizzBuzz 15 :~: 'FizzBuzz
+divisibleBy3And5 = Refl
